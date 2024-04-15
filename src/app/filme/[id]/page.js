cdcc31515f86fcfa874895e122be97e0f732328b
Joaofaleiros <joaofaleirospaulo@gmail.com>
2024-04-15 20:07:55 -0300

    update: new page

--- src/app/filme/[id]/page.js
+++ src/app/filme/[id]/page.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useEffect, useState } from "react";
 import api from "@/tools/api";
+import Image from 'next/image';
 
 export default function Filme({ params }) {
 
@@ -13,7 +14,7 @@ export default function Filme({ params }) {
       api
             .get(`/3/movie/${id}`)
             .then((resposta) => {
-                setFilme(resposta.data.results);
+                setFilme(resposta.data);
             })
             .catch((error) => {
                 console.log(error);
@@ -23,27 +24,21 @@ export default function Filme({ params }) {
         getFilme();
     }, []);
 
+    console.log(filme)
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {filme.map((filme) => (
-          <article
-            key={filme.id}
-            className="flex flex-col"
-            onClick={() => {
-              router.push(`/filme/${filme.id}`);
-            }}
-          >
-            <Image
-              src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`}
-              alt={filme.title}
-              width={500}
-              height={750}
-            />
-            <h2 className="text-xl font-semibold">{filme.title}</h2>
-            <p>{filme.overview}</p>
-          </article>
-        ))}
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+            <section className="flex min-h-screen justify-center">
+                <article className="flex flex-col items-center">
+                    <Image
+                        src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`}
+                        alt={filme.title}
+                        width={300}
+                        height={300}
+                        />
+                    <h2 className="text-xl font-semibold">{filme.title}</h2>
+                    <p>{filme.overview}</p>
+                </article>
             </section>
         </main>
     )
